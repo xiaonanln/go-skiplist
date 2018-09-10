f61cb8611d83e49ed988fd38594d23c8881a57b2
Seis <xiaonanln@gmail.com>
2018-09-10 08:32:35 +0800

    memory test

---
+++ memory_test.go
@@ -0,0 +1,41 @@
+package skiplist
+
+import (
+	"os"
+	"runtime/pprof"
+	"testing"
+
+	"github.com/petar/GoLLRB/llrb"
+)
+
+const (
+	testSize = 10000000
+)
+
+func TestMemorySkipList(t *testing.T) {
+	fd, err := os.OpenFile("skiplist.heap.pprof", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	tree := New()
+	for i := 0; i < testSize; i++ {
+		tree.ReplaceOrInsert(Int(i))
+	}
+
+	pprof.WriteHeapProfile(fd)
+}
+
+func TestMemoryLLRB(t *testing.T) {
+	fd, err := os.OpenFile("llrb.heap.pprof", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	tree := llrb.New()
+	for i := 0; i < testSize; i++ {
+		tree.ReplaceOrInsert(llrb.Int(i))
+	}
+
+	pprof.WriteHeapProfile(fd)
+}
