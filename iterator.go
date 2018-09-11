c7757be1de7b97bb97ea3a14e6222b69b2fbbd5d
Seis <xiaonanln@gmail.com>
2018-09-11 08:58:44 +0800

    cleanup code

--- iterator.go
+++ iterator.go
@@ -2,15 +2,18 @@ package skiplist
 
 type ItemIterator func(i Item) bool
 
+// Ascend will call iterator once for each element in ascending order.
+// It will stop whenever the iterator returns false.
 func (sl *SkipList) Ascend(iterator ItemIterator) {
-	var end *node
-	for n := get_forward(sl.head, 0); n != end; n = get_forward(n, 0) {
+	for n := get_forward(sl.head, 0); n != nil; n = get_forward(n, 0) {
 		if !iterator(n.Item) {
 			return
 		}
 	}
 }
 
+// AscendRange will call iterator once for each element greater or equal to `greaterOrEqual` but less than `lessThan`
+// in ascending order. It will stop whenever the iterator returns false.
 func (sl *SkipList) AscendRange(greaterOrEqual, lessThan Item, iterator ItemIterator) {
 	p := sl.head
 	var end *node
